3f751c32d9f25c1e7f733d67c5c7653a6e169dce
Yinan Xu <xuyinan@ict.ac.cn>
2023-11-27 19:11:08 +0800

    mul: reduce one cycle latency

--- playground/src/noop/muldiv.scala
+++ playground/src/noop/muldiv.scala
@@ -16,26 +16,8 @@ class MUL extends Module{
     val io = IO(new MULIO)
 
     val out_r       = RegInit(0.U(32.W))
-    val val1        = RegInit(0.U(32.W))
-    val val2        = RegInit(0.U(32.W))
     val valid_r     = RegInit(false.B)
-    val sIdle :: sBusy :: Nil = Enum(2)
-    val state = RegInit(sIdle)
     io.out := out_r
-    io.valid := valid_r
-    out_r := (val1 * val2)(31, 0)
-    switch(state){
-        is(sIdle){
-            valid_r := false.B
-            when(io.en){
-                state := sBusy
-                val1 := io.a
-                val2 := io.b
-            }
-        }
-        is(sBusy){
-            valid_r := true.B
-            state := sIdle
-        }
-    }
+    io.valid := RegNext(io.en)
+    out_r := (io.a(31, 0) * io.b(31, 0))(31, 0)
 }
